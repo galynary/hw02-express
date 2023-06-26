f2ca9cd09c8b3aba341f8868d34f78ae28aec605
galynary <107271972+galynary@users.noreply.github.com>
2023-06-26 17:17:23 +0300

    change

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,9 @@
 const express = require("express");
+
 const router = express.Router();
+
 const ctrl = require("../../controllers/contacts");
+
 const { validateBody, isValidId } = require("../../middlewares");
 
 router.get("/", ctrl.listContacts);
