cf3bd63b244f034a77cb99c63e006d1c042298bc
galynary <107271972+galynary@users.noreply.github.com>
2023-06-27 22:19:42 +0300

    add newUser

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,13 @@
 const express = require("express");
+
 const ctrl = require("../../controllers/auth");
 
 const { validateBody } = require("../../middlewares");
-const {schemas}=require("../../models/auth")
+
+const { schemas } = require("../../models/auth");
 
 const router = express.Router();
-router.post("/register", validateBody(schemas.registerschema), ctrl.register)
+
+router.post("/register", validateBody(schemas.registerschema), ctrl.register);
 
 module.export = router;
