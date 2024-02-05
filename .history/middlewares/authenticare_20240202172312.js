1524b9b2f6fd3d8393c72d083be2559bcb20cb5d
galynary <107271972+galynary@users.noreply.github.com>
2024-02-05 10:30:27 +0200

    change

--- .history/middlewares/authenticare_20240202172312.js
+++ .history/middlewares/authenticare_20240202172312.js
@@ -9,13 +9,13 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, res, next) => {
 	const { authorization = "" } = req.headers;
 	const [bearer, token] = authorization.split(" ");
-	if (bearer !== "Bearer") {   /* перевірка  чи ми авторизовані*/
+	if (bearer !== "Bearer") {
 		next(HttpError(401));
 	}
 	try {
 		const { id } = jwt.verify(token, SECRET_KEY);
 		const user = await User.findById(id);
-		if (!user || !user.token || user.token !== token) {       /* перевірка  чи це справді ми*/
+		if (!user || !user.token || user.token !== token) {
 			next(HttpError(401, "Not authorized"));
 		}
 		req.user = user;
